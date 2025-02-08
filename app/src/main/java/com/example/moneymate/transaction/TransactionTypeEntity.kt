333b13eed534a3ae9ad483aad535172e88281243
chain13 <chain13@email.com>
2025-02-09 01:38:46 +0700

    hotfix TransactionTypeEntity.kt

--- app/src/main/java/com/example/moneymate/transaction/TransactionTypeEntity.kt
+++ app/src/main/java/com/example/moneymate/transaction/TransactionTypeEntity.kt
@@ -4,11 +4,9 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-class TransactionTypeEntity {
-    @Entity(tableName = "transaction_type")
-    data class TransactionType(
-        @PrimaryKey(autoGenerate = true) val id: Long = 0,
-        @ColumnInfo(name = "type_name") val typeName: String
-    )
+@Entity(tableName = "transaction_type")
+data class TransactionType(
+    @PrimaryKey(autoGenerate = true) val id: Long = 0,
+    @ColumnInfo(name = "type_name") val typeName: String
+)
 
-}
