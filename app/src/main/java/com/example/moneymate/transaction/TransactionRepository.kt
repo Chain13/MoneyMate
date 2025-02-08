bebe73ebd18ba197f756d34e5aff7dc4b2939746
chain13 <chain13@email.com>
2025-02-09 01:06:01 +0700

    Removed all no used function

--- app/src/main/java/com/example/moneymate/transaction/TransactionRepository.kt
+++ app/src/main/java/com/example/moneymate/transaction/TransactionRepository.kt
@@ -1,6 +1,5 @@
 package com.example.moneymate.transaction
 
-import kotlinx.coroutines.flow.Flow
 
 class TransactionRepository(private val transactionDao: TransactionDao) {
 
@@ -8,21 +7,4 @@ class TransactionRepository(private val transactionDao: TransactionDao) {
         transactionDao.insertTransaction(transaction)
     }
 
-    suspend fun update(transaction: Transaction) {
-        transactionDao.updateTransaction(transaction)
-    }
-
-    suspend fun delete(transaction: Transaction) {
-        transactionDao.deleteTransaction(transaction)
-    }
-
-    fun getAllTransactions(): Flow<List<Transaction>> = transactionDao.getAllTransactions()
-
-    fun getTransactionsByType(type: String): Flow<List<Transaction>> = transactionDao.getTransactionsByType(type)
-
-    fun getTransactionsByCategory(category: String): Flow<List<Transaction>> = transactionDao.getTransactionsByCategory(category)
-
-    fun getTotalExpenses(): Flow<Double?> = transactionDao.getTotalExpenses()
-
-    fun getTotalIncome(): Flow<Double?> = transactionDao.getTotalIncome()
 }
