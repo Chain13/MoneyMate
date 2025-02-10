2bfbf562a7f9b820239b2fbd120dc3afd073ff7f
chain13 <chain13@email.com>
2025-02-10 21:26:15 +0700

    Polish Transaction Create

--- app/src/main/java/com/example/moneymate/transaction/view/TransactionCreateView.kt
+++ app/src/main/java/com/example/moneymate/transaction/view/TransactionCreateView.kt
@@ -3,10 +3,12 @@ package com.example.moneymate.transaction.view
 import android.app.Application
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.text.input.TextFieldLineLimits
 import androidx.compose.foundation.text.input.rememberTextFieldState
 import androidx.compose.foundation.text.input.setTextAndPlaceCursorAtEnd
@@ -16,88 +18,82 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuAnchorType
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableDoubleStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.input.pointer.motionEventSpy
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.NavHostController
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 import com.example.moneymate.transaction.entity.TransactionTypeEntity
 import com.example.moneymate.transaction.viewModel.TransactionCreateViewModel
-import com.example.moneymate.R
+
 @Composable
 fun TransactionCreateView(
     viewModel: TransactionCreateViewModel,
-    navController: NavHostController,
-    modifier: Modifier
+    navController: NavController,
+    modifier: Modifier = Modifier
 ) {
-    val amount = viewModel.amount.collectAsState()
-    val category = viewModel.category.collectAsState()
     val allType = viewModel.allType.observeAsState(listOf())
-    val isSavedTransaction = viewModel.isTransactionSaved.collectAsState()
-    val isAmountError = viewModel.isError.collectAsState()
-    Column(
-        modifier = modifier
-            .fillMaxSize()
-            .padding(16.dp), horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.spacedBy(20.dp)
-    ) {
-        Text("Create Transaction", style = MaterialTheme.typography.headlineMedium)
+    TransactionCreateScreen(
+        modifier,
+        saveTransaction = viewModel::saveTransaction,
+        allType = allType.value,
+        navController = navController
+    )
+}
+@Composable
+fun TransactionCreateScreen(modifier: Modifier = Modifier, saveTransaction: (Double, String, TransactionTypeEntity?)-> Unit, allType:List<TransactionTypeEntity>, navController: NavController, ) {
+    var amount by rememberSaveable { mutableStateOf("") }
+    var category by rememberSaveable { mutableStateOf("") }
+    var type by remember { mutableStateOf<TransactionTypeEntity?>(null) }
+    var isTransactionSaved by rememberSaveable { mutableStateOf(false) }
+    Column(modifier=modifier
+        .fillMaxSize()
+        .padding(16.dp), horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.spacedBy(20.dp)) {
+        Text("Transaction Create", style = MaterialTheme.typography.headlineMedium)
         OutlinedTextField(
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-            value = amount.value,
-            onValueChange = {viewModel.setAmount(it)},
-            label = { Text("Amount") },
-            isError = isAmountError.value,
-            supportingText = {
-                if(isAmountError.value) {
-                    Text(
-                        modifier = Modifier.fillMaxWidth(),
-                        text = "Amount must contain only number",
-                        color = MaterialTheme.colorScheme.error
-                    )
-                }
-            },
-            trailingIcon = {
-                if (isAmountError.value)
-                    Icon(
-                        painter = painterResource(R.drawable.baseline_error_24),
-                        contentDescription = "Amount Error", tint = MaterialTheme.colorScheme.error)
-
-            }
+            value = amount,
+            onValueChange = { amount=it },
+            label = { Text("Transaction Amount") },
+            modifier = Modifier.fillMaxWidth(),
+            placeholder = { Text("Enter type (Expense, Income, Transfer)") }
         )
         OutlinedTextField(
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
-            value = category.value,
-            onValueChange = { viewModel.setCategories(it) },
-            label = { Text("Categories") })
+            value = category,
+            onValueChange = { category=it },
+            label = { Text("Transaction Category") },
+            modifier = Modifier.fillMaxWidth(),
+            placeholder = { Text("Enter type (Expense, Income, Transfer)") }
+        )
         TransactionTypeDropdown(
-            allType = allType.value,
-            viewModel = viewModel,
-            modifier = Modifier
+            allType = allType,
+            modifier = modifier,
+            setType = {it->type=it}
         )
         Button(
             onClick = {
-                viewModel.saveTransaction()
-
+                saveTransaction(amount.toDouble(), category,type)
+                isTransactionSaved = true
             },
             modifier = Modifier.fillMaxWidth()
         ) {
-            Text("Save Transaction")
+            Text("Submit")
         }
         Button(
             onClick = {
@@ -107,18 +103,20 @@ fun TransactionCreateView(
         ) {
             Text("Cancel")
         }
-        if (isSavedTransaction.value) {
+        Spacer(modifier = Modifier.height(20.dp))
+
+        // Success Message after Saving
+        if (isTransactionSaved) {
             Text("Transaction Saved Successfully!", color = MaterialTheme.colorScheme.primary)
         }
     }
 }
-
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TransactionTypeDropdown(
-    viewModel: TransactionCreateViewModel,
     allType: List<TransactionTypeEntity>,
-    modifier: Modifier
+    modifier: Modifier,
+    setType: (TransactionTypeEntity) -> Unit
 ) {
     var expanded by remember { mutableStateOf(false) }
     val textFieldState = rememberTextFieldState("")
@@ -146,7 +144,7 @@ fun TransactionTypeDropdown(
                     onClick = {
                         textFieldState.setTextAndPlaceCursorAtEnd(option.typeName)
                         expanded = false
-                        viewModel.setType(option.id.toString())
+                        setType(option)
                     },
                     contentPadding = ExposedDropdownMenuDefaults.ItemContentPadding,
                 )
@@ -156,7 +154,19 @@ fun TransactionTypeDropdown(
 
 
 }
-
+@Preview(showSystemUi = true)
+@Composable
+fun TransactionCreateScreenPreview() {
+    TransactionCreateScreen(
+        modifier = Modifier.statusBarsPadding(),
+        saveTransaction = { _, _, _ -> },
+        allType = listOf(TransactionTypeEntity(
+            id = 1,
+            typeName = "Education"
+        )),
+        navController = rememberNavController()
+    )
+}
 class TransactionCreteViewModelFactory(val application: Application) :
     ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
